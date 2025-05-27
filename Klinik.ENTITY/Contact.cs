8ae0d557b76b06b9e95409488940817126d2e55b
Eymen <yamaneymen12345@gmail.com>
2025-05-27 22:20:29 +0300

    Yeni Claaslar Eklendi Sayfa Türkce Diline Çeviriliyor

---
+++ Klinik.ENTITY/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinik.ENTITY
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [StringLength(500)]
+        public string Address { get; set; }
+        
+        [Required(ErrorMessage = "Telefon numarası boş bırakılamaz.")]
+        [RegularExpression(@"^0[0-9]{10}$", ErrorMessage = "Telefon numarası 0 ile başlamalı ve 11 haneli olmalıdır.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "E-posta adresi boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi giriniz.")]
+        public string Email { get; set; }
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string X { get; set; }
+        public string Linkedin { get; set; }
+
+    }
+}
---
+++ Klinik.ENTITY/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinik.ENTITY
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string İmageUrl { get; set; }
+        public string Department { get; set; }
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string X { get; set; }
+
+    }
+}
